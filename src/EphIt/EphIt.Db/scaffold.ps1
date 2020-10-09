f0ba3ffcd5967d689c4b686f6e8c006c1ac273e3
Ryan Ephgrave <ryan2065@gmail.com>
2020-10-09 15:12:30 -0500

    moved files around

--- src/EphIt/EphIt.Db/scaffold.ps1
+++
@@ -1,6 +0,0 @@
-Get-ChildItem "$PsScriptRoot\Models" -Filter '*.cs' | Remove-Item -Force
-
-Push-Location $PsScriptRoot
-
-dotnet ef dbcontext scaffold "Server=Lab-CM.Home.Lab;Database=EphIt;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer --startup-project ..\EphIt.Service -o Models
-
