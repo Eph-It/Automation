69691fb2ffd669d5682e327311fdc13219d94289
Ryan Ephgrave <ryan2065@gmail.com>
2021-01-09 15:52:41 -0600

    StartingEphItUI

---
+++ src/EphIt/Classlibraries/EphIt.UI/Script/IScriptClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EphIt.UI.Script
+{
+    public interface IScriptClient
+    {
+    }
+}
---
+++ src/EphIt/Classlibraries/EphIt.UI/Script/ScriptClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Simple.OData.Client;
+
+namespace EphIt.UI.Script
+{
+    public class ScriptClient : IScriptClient
+    {
+        private ODataClient _odataClient;
+        public ScriptClient(ODataClient odataClient)
+        {
+            _odataClient = odataClient;
+        }
+
+    }
+}
