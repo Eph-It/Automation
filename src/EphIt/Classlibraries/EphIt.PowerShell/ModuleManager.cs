e60f121bf78600f425592e27fe08ba627a4a63e4
Fred Bainbridge <Fred@teambainbridge.com>
2021-02-25 07:12:38 -0600

    validing module

--- src/EphIt/Classlibraries/EphIt.PowerShell/ModuleManager.cs
+++ src/EphIt/Classlibraries/EphIt.PowerShell/ModuleManager.cs
@@ -3,7 +3,9 @@ using System.IO.Compression;
 using System.IO;
 using EphIt.BL.Automation;
 using System.Management.Automation;
-
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Management.Automation.Runspaces;
 
 namespace EphIt.PowerShell
 {
@@ -14,17 +16,21 @@ namespace EphIt.PowerShell
         {
             tempDirectory = automationHelper.GetTempDirectory();
         }
-        public VMModule NewModule(byte[] compressedModule)
+        public VMModule NewModule(byte[] compressedModule, string moduleName)
         {
-            UnzipModule(compressedModule);
+            UnzipModule(compressedModule, moduleName);
             //validate its a valid powershell module TODO
             //get the name of the module TODO
             //save to database / update TODO
             return (new VMModule(new Module()));
         }
-        public string UnzipModule(byte[] compressedModule)
+        public string UnzipModule(byte[] compressedModule, string moduleName)
         {
-            string tmpExtractPath = tempDirectory + "\\" + System.Guid.NewGuid().ToString();
+            string tmpExtractPath = tempDirectory + "\\" + moduleName;
+            if (Directory.Exists(tmpExtractPath))
+            {
+                Directory.Delete(tmpExtractPath, true);
+            }
             var zippedStream = new MemoryStream(compressedModule);
             var archive = new ZipArchive(zippedStream);
             archive.ExtractToDirectory(tmpExtractPath, true);
@@ -35,10 +41,31 @@ namespace EphIt.PowerShell
             return null;
         }
 
-        public bool ValidatePowerShellModule(string path)
+        public PSModuleInfo GetPowerShellModuleInfo(string path, string name)
         {
-        //essentially do this.
-        //https://github.com/PowerShell/PowerShell/blob/master/src/System.Management.Automation/engine/Modules/TestModuleManifestCommand.cs
+            System.Management.Automation.PowerShell ps = System.Management.Automation.PowerShell.Create();
+            Collection<PSObject> results = new Collection<PSObject>();
+            ps.AddScript("Set-ExecutionPolicy Unrestricted");  //manage this better somehow
+            ps.AddScript("$VerbosePreference = 'Continue'");
+            ps.AddScript($"Import-Module -Name {path} -Verbose");
+            ps.AddScript("Get-Module -Name " + name + " -Verbose;");
+            results = ps.Invoke();
+            ps.Dispose();
+            foreach(var result in results) {
+                if(result.TypeNames.Contains("System.Management.Automation.PSModuleInfo")) {
+                    return (PSModuleInfo)result.BaseObject;
+                }
+            }
+            return null;
+        }
+        public bool ValidatePowerShellModule(string path, string name) {
+            //add version, etc someday if there is ever a need.
+            PSModuleInfo moduleInfo = GetPowerShellModuleInfo(path, name);
+            if(moduleInfo != null) {
+                if(moduleInfo.Name.Equals(name)) {
+                    return true;
+                }
+            }
             return false;
         }
     }
--- src/EphIt/EphIt.Tests/EphIt.PowerShell.Tests.cs
+++ src/EphIt/EphIt.Tests/EphIt.PowerShell.Tests.cs
@@ -21,37 +21,42 @@ namespace EphIt.PowerShell.Tests
             automationHelper.SetTempDirectory();
         }
 
-        public byte[] CreateModule()
+        public byte[] CreateModule(string moduleName)
         {
             List<string> psm1BodyStrings = new List<string>();
             psm1BodyStrings.Add("Function Get-Widget{}; Export-ModuleMember 'Get-Widget'");
-            string psm1Path = "tmpModule\\tmpModule.psm1";
-            Directory.CreateDirectory("tmpModule");
+            string psm1Path = "tmpModule\\" + moduleName + ".psm1";
+            Directory.CreateDirectory(moduleName);
             File.Create(psm1Path).Dispose();
             File.WriteAllLines(psm1Path, psm1BodyStrings);
-            if (File.Exists("tmpModule.zip"))
+            if (File.Exists(moduleName + ".zip"))
             {
-                File.Delete("tmpModule.zip");
+                File.Delete(moduleName + ".zip");
             }
-            ZipFile.CreateFromDirectory("tmpModule", "tmpModule.zip");
-            Directory.Delete("tmpModule", true);
-            return File.ReadAllBytes("tmpModule.zip");
+            ZipFile.CreateFromDirectory(moduleName, moduleName + ".zip");
+            Directory.Delete(moduleName, true);
+            return File.ReadAllBytes(moduleName + ".zip");
         }
+        
         [TestMethod]
         public void ShouldUnzipToTemp(){
             ModuleManager mm = new ModuleManager(automationHelper);
-            string tmpDestination = mm.UnzipModule(CreateModule());
+            string tmpDestination = mm.UnzipModule(CreateModule("tmpModule"), "tmpModule");
             Assert.IsTrue(Directory.Exists(tmpDestination));
             File.Delete("tmpModule.zip");
             Directory.Delete(tmpDestination,true);
         }
 
         [TestMethod]
-        public void ShouldIdentityProperPowerShellModule(){}
-
+        public void ShouldValidateProperPowerShellModule(){
+            ModuleManager mm = new ModuleManager(automationHelper);
+            string tmpDestination = mm.UnzipModule(CreateModule("tmpModule"), "tmpModule");
+            var success = mm.ValidatePowerShellModule(tmpDestination, "tmpModule");
+            Assert.IsTrue(success);
+        }
 
         [TestMethod]
-        public void ShouldIdentityImroperPowerShellModule(){}
+        public void ShouldNotValidateImroperPowerShellModule(){}
 
     }
 }
